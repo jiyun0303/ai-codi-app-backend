f31326dc2d5cdd07c3118a482968d67464624ee9
jiyun0303 <sss7470026@gmail.com>
2025-09-29 18:51:51 +0900

    Add files via upload

---
+++ ai-codi-app/src/main/java/com/example/ai_codi_app/AiCodiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_codi_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodiAppApplication.class, args);
+	}
+
+}
---
+++ ai-codi-app/src/test/java/com/example/ai_codi_app/AiCodiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_codi_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
